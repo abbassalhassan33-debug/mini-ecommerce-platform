ef2a605393972c02b25c94c7be744aad98522cbd
Abbass Alhassan <abbassalhassan66@gmail.com>
2026-08-02 15:46:46 +0300

    Protect product routes with JWT

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
+import ProductListPage from "./pages/ProductListPage";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
+
+      <Route
+        path="/products"
+        element={
+          <ProtectedRoute>
+            <ProductListPage />
+          </ProtectedRoute>
+        }
+      />
       <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   );
